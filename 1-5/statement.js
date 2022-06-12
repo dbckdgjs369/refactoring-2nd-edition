d3822f9a8286a1ffb0b122d7d9ea78232a250f55
yoo chang heon <dbckdgjs1130@naver.com>
2022-06-13 00:25:18 +0900

    HTML 버전 작성 준비

--- 1-5/statement.js
+++ 1-5/statement.js
@@ -1,71 +1,8 @@
+import createStatement from "./createStatementData";
+
 function statement(invoice, plays) {
   return renderPlainText(createStatement(invoice, plays));
 }
-
-function createStatement(invoice, plays) {
-  const statementData = {}; //중간 데이터 구조 인수로 전달
-  statementData.customer = invoice.customer; //고객데이터를 중간 데이터로 옮김
-  statementData.performances = invoice.performances.map(enrichPerformance);
-  statementData.totalAmount = totalAmount(statementData);
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-  return statementData;
-}
-
-function enrichPerformance(aPerformance) {
-  const result = Object.assign({}, aPerformance);
-  result.play = playFor(result);
-  result.amount = amountFor(result);
-  result.volumeCredits = volumeCreditsFor(result);
-  return result;
-}
-function playFor(aPerformance) {
-  //playFor 함수를 옮김
-  return plays[aPerformance.playID];
-}
-
-function amountFor(aPerformance) {
-  let result = 0;
-  switch (aPerformance.play.type) {
-    case "tragedy":
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy":
-      result = 30_000;
-
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-
-    default:
-      throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-  }
-  return result;
-}
-
-function totalAmount(data) {
-  return data.performances.reduce((total, p) => total + p.amount, 0);
-}
-
-function totalVolumeCredits(data) {
-  return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
-}
-
-function volumeCreditsFor(aPerformance) {
-  let result = 0;
-  result += Math.max(aPerformance.audience - 30, 0);
-
-  if ("comedy" === aPerformance.play.type) {
-    result += Math.floor(aPerformance.audience / 5);
-  }
-
-  return result;
-}
-
 function renderPlainText(data, plays) {
   let result = `청구내역 (고객명: ${data.customer})\n`; //고객데이터를 중간 데이터로 부터 얻기
   for (let perf of data.performances) {
@@ -73,6 +10,27 @@ function renderPlainText(data, plays) {
   }
   result += `총액 ${usd(data.totalAmount)}\n`;
   result += `적립 포인트 ${data.totalVolumeCredits}점\n`;
+  return result;
+}
+
+function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+  let result = `<h1>청구 내역 ${data.customer}</h1>\n`;
+
+  result += "<table>\n";
+  result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>";
+
+  for (let perf of data.performances) {
+    result += `  <tr><td>${perf.play.name}</td><td>${perf.audience}석</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>\n`;
+  }
+  result += "</table>\n";
+  result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>적립포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
+
   return result;
 }
 
